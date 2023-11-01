bb33d90ce7cf4112ec75472b532482cdc795adc7
BizuhanAb <bizuhan06@gmail.com>
2023-11-01 12:43:21 +0530

    modification added to addition file

--- addition.py
+++ addition.py
@@ -17,9 +17,6 @@ Run python autograder.py
 """
 def add(a, b):
     "Return the sum of a and b"
-    "*** YOUR CODE HERE ***"
-    a=1
-    b=1
     print("Passed a = %s and b = %s, returning a + b = %s" % (a, b, a + b))
     return a + b
 
